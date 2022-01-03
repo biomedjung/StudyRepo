007872da0155a88d63efa909c9250ecef1c0966a
Guilherme Jung <27732278+biomedjung@users.noreply.github.com>
2022-01-03 06:56:05 -0300

    Delete des010.py

--- pynotes/des010.py
+++
@@ -1,4 +0,0 @@
-# Converta um valor em Real para Dólar
-real = float(input('Quantos reais você quer converter em dólar? '))
-dolar = real * 0.178
-print('BRL {:.2f} = USD {:.2f}'.format(real, dolar))
