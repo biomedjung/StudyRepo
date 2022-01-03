ffa07580df80e8fb78b8c949da9b9d36d956994a
Guilherme Jung <27732278+biomedjung@users.noreply.github.com>
2022-01-03 06:53:27 -0300

    Delete des003.py

--- pynotes/des003.py
+++
@@ -1,5 +0,0 @@
-# crie um programa que leia dois números e mostre a soma entre eles
-n1 = float(input('Digite um número: '))
-n2 = float(input('Digite outro número: '))
-s = n1 + n2
-print('A soma entre {0} e {1} é igual a {2}'.format(n1, n2, s))
