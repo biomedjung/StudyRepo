6f22abed9ab6cf0f9d989af8dd32e82186ccffa2
Guilherme Jung <27732278+biomedjung@users.noreply.github.com>
2022-01-03 06:55:47 -0300

    Delete des008.py

--- pynotes/des008.py
+++
@@ -1,5 +0,0 @@
-# lê um valor em metros, converte em cm e mm
-m = float(input('Digite a sua medida em metros: '))
-cm = m * 100
-mm = m * 1000
-print('Sua medida de {} m equivale a {} cm e {} mm'.format(m, cm, mm))
