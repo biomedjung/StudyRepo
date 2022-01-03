d7e27b535f35386ff6ea79a04f5564dc51aac29f
Guilherme Jung <27732278+biomedjung@users.noreply.github.com>
2022-01-03 06:54:32 -0300

    Delete des005.py

--- pynotes/des005.py
+++
@@ -1,5 +0,0 @@
-# Montar um programa que pede um número, mostra seu antecessor e seu sucessor
-n = int(input('Digite um valor inteiro: '))
-ant = n - 1
-suc = n + 1
-print('O antecessor de {} vale {}, e o sucessor vale {}'.format(n, ant, suc))
