5b7848fd327e5e18e8a06d088ac203d31c1ed919
Guilherme Jung <27732278+biomedjung@users.noreply.github.com>
2022-01-03 06:59:37 -0300

    Delete des015.py

--- pynotes/des015.py
+++
@@ -1,9 +0,0 @@
-# Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado.
-# Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
-
-dist = float(input('Digite a distância percorrida: '))
-dia = float(input('Digite a quantidade de dias alugados: '))
-alug = dia * 60
-fuel = dist * 0.15
-pag = alug + fuel
-print('O valor total a ser pago, BRL{} de aluguel + BRL{} de combustivel, somam BRL{}'.format(alug, fuel, pag))
