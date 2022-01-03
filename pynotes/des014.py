4a2a94dcefa6da7a71e859095184544c41e48451
Guilherme Jung <27732278+biomedjung@users.noreply.github.com>
2022-01-03 06:58:34 -0300

    Delete des014.py

--- pynotes/des014.py
+++
@@ -1,5 +0,0 @@
-# converção de ºC para ºF
-c = float(input('Digite a temperatura em Celsius: '))
-f = c * 1.8 + 32
-# cada grau Fahrenheit equivale a 1.8 Celcius, e 32 graus de diferença
-print('{} ºC = {} ºF'.format(c, f))
