f0c82badecc7cfcb6cfb2016dc0c9b5e83e56da7
Guilherme Jung <27732278+biomedjung@users.noreply.github.com>
2022-01-03 06:55:39 -0300

    Delete des007.py

--- pynotes/des007.py
+++
@@ -1,7 +0,0 @@
-# Duas notas de um aluno, calcula e mostra a média
-# confirmar o resultado!!! ordem de precedencia
-print('Olá, a minha função é calcular a média de duas avaliações do seu aluno!')
-n1 = float(input('Digite um valor: '))
-n2 = float(input('Digite outro valor: '))
-med = (n1 + n2)/2
-print('A média das notas vale {}'.format(med))
