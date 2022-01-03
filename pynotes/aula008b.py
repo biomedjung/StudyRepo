d604dfbec6853fbdac53679c6d3425499677d3c8
Guilherme Jung <27732278+biomedjung@users.noreply.github.com>
2022-01-03 06:50:51 -0300

    Delete aula008b.py

--- pynotes/aula008b.py
+++
@@ -1,9 +0,0 @@
-import random, emoji
-num = random.random()  # float entre zero e um
-numint = random.randint(1, 10)  # intervalo entre parenteses
-print('random float (0 - 1): '.format(num), num)  # não entendi pq o format bugou......????
-print('random integer (1 - 10): '.format(numint), numint)
-print(num)
-print(numint)
-print(emoji.emojize('Olá mundo ! :bell:', use_aliases=True))
-print(emoji.emojize(':biohazard:'))
